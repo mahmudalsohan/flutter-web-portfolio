f3355cca1031ba2f143f192f2f931fa16e1deccb
Mahmud218 <73057540+Mahmud218@users.noreply.github.com>
2021-03-18 10:35:11 +0600

    changes identity

--- lib/pages/home/components/carousel_items.dart
+++ lib/pages/home/components/carousel_items.dart
@@ -14,7 +14,7 @@ List<CarouselItemModel> carouselItems = List.generate(
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
-            "PRODUCT DESIGNER",
+            "FLUTTER DEVELOPER",
             style: GoogleFonts.oswald(
               color: kPrimaryColor,
               fontWeight: FontWeight.w900,
@@ -25,7 +25,7 @@ List<CarouselItemModel> carouselItems = List.generate(
             height: 18.0,
           ),
           Text(
-            "MICHELE\nHARRINGTON",
+            "MAHMUD\nAL SOHAN",
             style: GoogleFonts.oswald(
               color: Colors.white,
               fontSize: 40.0,
@@ -37,7 +37,7 @@ List<CarouselItemModel> carouselItems = List.generate(
             height: 10.0,
           ),
           Text(
-            "Full-stack developer, based in Barcelona",
+            "based in Dhaka, Bangladesh",
             style: TextStyle(
               color: kCaptionColor,
               fontSize: 15.0,
@@ -51,7 +51,7 @@ List<CarouselItemModel> carouselItems = List.generate(
             child: Wrap(
               children: [
                 Text(
-                  "Need a full custom website?",
+                  "Need a full custom WEBSITE and APP?",
                   style: TextStyle(
                     color: kCaptionColor,
                     fontSize: 15.0,
@@ -63,7 +63,7 @@ List<CarouselItemModel> carouselItems = List.generate(
                   child: MouseRegion(
                     cursor: SystemMouseCursors.click,
                     child: Text(
-                      " Got a project? Let's talk.",
+                      " Let's talk.",
                       style: TextStyle(
                         height: 1.5,
                         color: Colors.white,
